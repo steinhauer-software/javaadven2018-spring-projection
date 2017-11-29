6076a5d5a12accd8c41ee0cfe4af19b8e3273116
daincredibleholg <me@h0lg.net>
2017-11-29 21:44:27 +0000

    Initial commit.

---
+++ src/main/java/software/steinhauer/springprojectiondemo/JavaAdvent2018SpringProjectionApplication.java
@@ -0,0 +1,12 @@
+package software.steinhauer.springprojectiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAdvent2018SpringProjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAdvent2018SpringProjectionApplication.class, args);
+	}
+}
